1d6231335a185679eff33634e5ad651c569794a0
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-12-17 23:28:20 +0200

    feat: enhance About page with detailed description and styled wrapper

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,30 @@
+import Wrapper from '../wrappers/AboutPage.js';
+
 const About = () => {
 	return (
-		<div>
-			<h2>About page</h2>
-		</div>
+		<Wrapper>
+			<h3>About Us</h3>
+			<p>
+				Introducing "Cocktail MixMaster," the ultimate party sidekick app that
+				fetches cocktails from the hilarious Cocktails DB API. With a flick of
+				your finger, you'll unlock a treasure trove of enchanting drink recipes
+				that'll make your taste buds dance and your friends jump with joy. Get
+				ready to shake up your mixology game, one fantastical mocktail at a
+				time, and let the laughter and giggles flow!
+			</p>
+
+			<p>
+				<a
+					href="https://www.thecocktaildb.com/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					https://www.thecocktaildb.com
+				</a>
+				&nbsp;— An open, crowd-sourced database of drinks and cocktails from
+				around the world.
+			</p>
+		</Wrapper>
 	);
 };
 
