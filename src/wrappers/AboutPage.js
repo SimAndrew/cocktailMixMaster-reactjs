9a5c4a5823cdadf92674dbff5daaea674f91d330
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-12-19 15:24:32 +0200

    feat: refactor AboutPage styles and wrap Outlet in HomeLayout with a section

--- src/wrappers/AboutPage.js
+++ src/wrappers/AboutPage.js
@@ -1,14 +1,10 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-	h3 {
-		padding-left: 1rem;
-	}
 	p {
 		line-height: 2;
 		color: var(--grey-500);
 		margin-top: 2rem;
-		padding: 0 1rem 0 1rem;
 	}
 `;
 
