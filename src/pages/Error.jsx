82286be3b1873a8b988e5ff120884ee60c9de602
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-12-19 15:48:56 +0200

    feat: add custom error handling page with 404 and general error messages

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,3 +1,29 @@
-const Error = () => <h2>Error page</h2>;
+import { Link, useRouteError } from 'react-router-dom';
+import Wrapper from '../wrappers/ErrorPage.js';
+import img from '../assets/not-found.svg';
+
+const Error = () => {
+	const error = useRouteError();
+
+	if (error.status === 404) {
+		return (
+			<Wrapper>
+				<div>
+					<img src={img} alt="not found" />
+					<h3>Ohh! page not found</h3>
+					<p>We can't seem to find the page you're looking for</p>
+					<Link to="/">back home</Link>
+				</div>
+			</Wrapper>
+		);
+	}
+	return (
+		<Wrapper>
+			<div>
+				<h3>Something went wrong!</h3>
+			</div>
+		</Wrapper>
+	);
+};
 
 export default Error;
