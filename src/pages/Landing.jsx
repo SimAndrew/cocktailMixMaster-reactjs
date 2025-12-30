8dc82980a5a9e8cfcb7002000b105030a61c968a
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-12-30 02:36:49 +0200

    feat: implement search form for cocktail search functionality

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,16 @@
 import { useLoaderData } from 'react-router-dom';
 import axios from 'axios';
 import CocktailList from '../components/CocktailList.jsx';
+import SearchForm from '../components/SearchForm.jsx';
 
 const cocktailSearchUrl =
 	'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-export const loader = async () => {
-	const searchTerm = 'a';
+// eslint-disable-next-line react-refresh/only-export-components
+export const loader = async ({ request }) => {
+	const url = new URL(request.url);
+
+	const searchTerm = url.searchParams.get('search') || 'coffee';
 	const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
 
 	return { drinks: response.data.drinks, searchTerm };
@@ -17,6 +21,7 @@ const Landing = () => {
 
 	return (
 		<>
+			<SearchForm searchTerm={searchTerm} />
 			<CocktailList drinks={drinks} />
 		</>
 	);
